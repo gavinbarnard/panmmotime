a17f2a040ee34a9165202f3b036666dab8f1ba7c
Gavin R. Barnard <gavin.r.barnard@gmail.com>
2025-02-12 19:28:43 -0500

    Update pantime.py
    print on single line
    add minutes to day/night

--- pantime.py
+++ pantime.py
@@ -6,14 +6,16 @@ from time import time, sleep
 from math import floor
 
 def find_time():
-    #initial_epoch = 1739274274
-    # Feb 11 2025 11:44:34 = 07:00 game time
+    # these are for aevos-1 
+    # avp_epoch = 1739273986 - out of date
+    
+    thronefast_epoch = 1739400236
 
-    initial_epoch = 1739273986
-    # FEB 11 2025 11:39:46 = 06:00 game time
+ 
 
     current_time = floor(time())
 
+    initial_epoch = thronefast_epoch
 
     day_hour = (4*60)+48
     night_hour = (2*60)+24
@@ -23,28 +25,36 @@ def find_time():
 
     fullday= day+night
     
+    night_time = None
+    day_time = None
     difference = current_time - initial_epoch
     days = floor(difference / fullday)
     d_difference = difference - (days * fullday)
     if d_difference > day: 
         hours = 18
-        add_hours = floor((d_difference - day ) / night_hour)
-        night_time = None
+        oadd_hours = floor((d_difference - day ) / night_hour)
+        add_hours = oadd_hours
         if add_hours >= 6:
             night_time = add_hours - 6
         else:
             night_time = hours + add_hours
         remainder = ((d_difference - day) / night_hour) - add_hours
+        until_day = floor(((12 - oadd_hours - remainder)*night_hour)/60)
         minutes = floor(remainder * 60)
-        print(f"it is night_time {night_time:02d}:{minutes:02d} and days since epoch {days}")
+        print(f"it is night_time {night_time:02d}:{minutes:02d} about {floor(until_day)} minutes until day and {days} days since epoch", end="\r", flush=True)
     elif d_difference < day:
         hours = 6
-        day_time = None
         add_hours = floor(d_difference / day_hour)
         remainder = (d_difference / day_hour) - add_hours
+        until_night = floor(((12 - add_hours - remainder)*day_hour)/60)
         minutes = floor(remainder * 60)
         day_time = hours + add_hours
-        print(f"it is day_time {day_time:02d}:{minutes:02d} and days since epoch {days}")
+        print(f"it is day_time {day_time:02d}:{minutes:02d} about {floor(until_night)} minutes until night and {days} days since epoch", end="\r", flush=True)
+
+    if day_time:
+        pass
+    elif night_time:
+        pass
 
 if __name__ == "__main__":
     while True:
